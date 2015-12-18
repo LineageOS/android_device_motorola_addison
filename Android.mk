0c78b1b8b8d1492206ed89cd48cf4ff0cf681eb7
Andrew Wheeler <awheeler@motorola.com>
2015-12-18 16:25:37 -0600

    IKQCOMSD1-3106 Add basic support for addison
    Addison's sensor hub is very 8996-based, so kick off the
    addison configuration with the current 8996 configuration.
    
    Change-Id: Idbdbab416209b1adaca17bb9130557d725680ac6
    Reviewed-on: http://gerrit.mot.com/808552
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Robert Wiegand <Robert.P.Wiegand@motorola.com>

--- Android.mk
+++ Android.mk
@@ -52,10 +52,22 @@ ifeq ($(BOARD_USES_MOT_SENSOR_HUB), true)
                 # Expose IR raw data for non-user builds
                 SH_CFLAGS += -D_ENABLE_RAW_IR_DATA
             endif
-        endif
-
+		# Addison -- may be 8952-based
+        else ifneq (,$(filter addison_%, $(strip $(TARGET_PRODUCT))))
+            SH_MODULE := motosh
+            SH_PATH := motosh_hal
+            SH_CFLAGS += -D_ENABLE_LA
+            SH_CFLAGS += -D_ENABLE_GR
+            SH_CFLAGS += -D_ENABLE_CHOPCHOP
+            SH_CFLAGS += -D_ENABLE_LIFT
+            SH_CFLAGS += -D_USES_BMI160_ACCGYR
+            SH_CFLAGS += -D_ENABLE_PEDO
+            ifneq (,$(filter userdebug eng,$(TARGET_BUILD_VARIANT)))
+                # Expose IR raw data for non-user builds
+                SH_CFLAGS += -D_ENABLE_RAW_IR_DATA
+            endif
         # 8952
-        ifeq ($(call is-board-platform,msm8952),true)
+        else ifeq ($(call is-board-platform,msm8952),true)
             SH_MODULE := stml0xx
             SH_PATH := stml0xx_hal
             SH_CFLAGS += -D_ENABLE_BMI160
