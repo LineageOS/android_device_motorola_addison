b4eb1480f1fe5ec20e0cbe96918ee0289dafb370
molg1 <molg1@lenovo.com>
2016-11-19 11:57:59 +0800

    IKSWN-14368 Sensor: Adjust Moto Glance Gesture sensor reporting mode
    - Change Moto Glance Gesture reporting mode from one-shot
      to special-trigger
    
    Change-Id: I9d98d3511982b72d33d6d611d4c0c5a21c7107a6
    Signed-off-by: molg1 <molg1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/921875
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Daniel Moore <dtmoore@motorola.com>
    Reviewed-by: Lijun Chen <chenlj2@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- stml0xx_hal/HubSensors.cpp
+++ stml0xx_hal/HubSensors.cpp
@@ -960,9 +960,6 @@ int HubSensors::readEvents(sensors_event_t* d, int dLen)
                     data->data[2] = 0;
                     data->timestamp = buff.timestamp;
                     data++;
-
-                    /* Disable, because this is a one shot sensor */
-                    setEnable(ID_MOTO_GLANCE_GESTURE, 0);
                 }
 
                 if (isHandleEnabled(ID_GLANCE_GESTURE)) {
