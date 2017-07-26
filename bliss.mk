44aae252490badb6542fa133aa78bf50256e4d1e
Kaneki <dinix.hot@gmail.com>
2017-07-26 10:34:08 -0300

    Update bliss.mk

--- bliss.mk
+++ bliss.mk
@@ -12,6 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 $(call inherit-product, device/motorola/addison/full_addison.mk)
 
 # Boot animation
