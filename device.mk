24ccdb2f62345711ff120104bfefca3019bfb4df
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-01-08 16:24:58 -0200

    albus: sepolicy: Remove leftovers from few services
    Change-Id: Ie6c5919dd90b8bb2bd298dfd809f93bb1afaf7bb

--- device.mk
+++ device.mk
@@ -296,9 +296,6 @@ PRODUCT_PACKAGES += \
 
 # Ramdisk
 PRODUCT_PACKAGES += \
-    init.mmi.touch.sh \
-    init.qcom.ril.sh \
-    init.qcom.bt.sh \
     init.qcom.fm.sh \
     wlan_carrier_bin.sh
 
@@ -308,7 +305,6 @@ PRODUCT_PACKAGES += \
     init.mmi.laser.sh \
     init.mmi.rc \
     init.mmi.usb.rc \
-    init.oem.hw.sh \
     init.qcom.rc \
     ueventd.qcom.rc
 
