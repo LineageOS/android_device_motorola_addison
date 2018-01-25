88730bcad8a0ad8d2fd8aad34824eaaa9abd5d3d
rahulsnair <rahulsnair30@gmail.com>
2018-01-25 13:16:51 +0100

    albus : build sensor hal
    Change-Id: I9f6552c9ee9b276abcae9058139d9d5d3ae3f390

--- device.mk
+++ device.mk
@@ -346,7 +346,8 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-service \
+    sensors.msm8953
 
 # Thermal
 PRODUCT_COPY_FILES += \
