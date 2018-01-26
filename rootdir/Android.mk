103465d5171e02a221f8cf84287b736a56ab66c7
Dan Pasanen <dan.pasanen@gmail.com>
2018-01-25 22:37:18 -0600

    albus: add init.gbmods.sh
    Change-Id: I2985b1ea82d3fbdd4a0c7f5b26b523ed00159bd3
    Signed-off-by: rahulsnair <rahulsnair30@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,5 +1,13 @@
 LOCAL_PATH:= $(call my-dir)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.gbmods.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_SRC_FILES    := bin/init.gbmods.sh
+LOCAL_VENDOR_MODULE    := true
+include $(BUILD_PREBUILT)
+
 # Configuration scripts
 
 include $(CLEAR_VARS)
