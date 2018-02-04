adf9edd24ae5ae2d5291a508b439756d29399a41
rahulsnair <rahulsnair30@gmail.com>
2018-02-04 20:29:17 +0100

    albus : remove init.gbmode.sh
    Change-Id: I3da9ab04c1d2dd5fb645e280343bde2892d9c38c

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -3,14 +3,6 @@ LOCAL_PATH:= $(call my-dir)
 # Configuration scripts
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.gbmods.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := EXECUTABLES
-LOCAL_SRC_FILES    := bin/init.gbmods.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.mmi.boot.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
