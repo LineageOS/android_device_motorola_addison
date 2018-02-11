f7febee541d58d5729bdb6350922d0591b61ab2f
rahulsnair <rahulsnair30@gmail.com>
2018-02-11 10:17:44 +0100

    albus : remove unwanted shims
    Change-Id: Ifa9bc4997956f98d37fec44070a7c72de590301c

--- device.mk
+++ device.mk
@@ -113,8 +113,7 @@ PRODUCT_PACKAGES += \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
-    libshim_skia \
-    libshims_camera
+    libshim_skia
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/camera/msm8953_mot_albus_camera.xml:system/etc/camera/msm8953_mot_albus_camera.xml \
