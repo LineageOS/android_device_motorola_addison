6d830451da735fb5263e6572f67e4a2180a74af5
Erfan Abdi <erfangplus@gmail.com>
2018-02-15 00:41:44 +0300

    albus: lineageactions: fix glance gesture
    * adapted with moto glance sensor
    
    This reverts commit bf0b97993697025766c9897e18f2c898178f0ceb.
    
    Change-Id: If0c0be1960219a086f494106ce68c5de4a9796b5
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- LineageActions/src/org/lineageos/settings/device/SensorHelper.java
+++ LineageActions/src/org/lineageos/settings/device/SensorHelper.java
@@ -37,6 +37,8 @@ public class SensorHelper {
     private static final int SENSOR_TYPE_MMI_FLAT_UP = 65537;
     private static final int SENSOR_TYPE_MMI_FLAT_DOWN = 65538;
     private static final int SENSOR_TYPE_MMI_STOW = 65539;
+    private static final int SENSOR_TYPE_MMI_GLANCE = 65548;
+    private static final int SENSOR_TYPE_MMI_GLANCE_APPROACH = 65555;
 
     private static final int BATCH_LATENCY_IN_MS = 100;
 
@@ -82,7 +84,11 @@ public class SensorHelper {
     }
 
     public Sensor getGlanceSensor() {
-        return mSensorManager.getDefaultSensor(Sensor.TYPE_GLANCE_GESTURE, true);
+        return mSensorManager.getDefaultSensor(SENSOR_TYPE_MMI_GLANCE, true);
+    }
+
+    public Sensor getApproachGlanceSensor() {
+        return mSensorManager.getDefaultSensor(SENSOR_TYPE_MMI_GLANCE_APPROACH, true);
     }
 
     public Sensor getProximitySensor() {
