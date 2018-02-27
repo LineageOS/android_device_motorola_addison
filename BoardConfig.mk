b592ea649636be9061b52959e6ddf4224aa5698e
rahulsnair <rahulsnair30@gmail.com>
2018-02-27 18:03:38 +0100

    albus : changes for kernel
    Change-Id: Ia585e36ab8fb3adab11910fa332877955d756ae2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -82,7 +82,7 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CONFIG := albus_defconfig
-TARGET_KERNEL_SOURCE := kernel/motorola/msm8953
+TARGET_KERNEL_SOURCE := kernel/motorola/albus
 
 # Audio
 AUDIO_FEATURE_ENABLED_ALAC_OFFLOAD := true
