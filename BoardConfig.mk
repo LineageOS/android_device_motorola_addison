dd5ff4460ad0ceb4539542675d880bfd8563670f
rahulsnair <rahulsnair30@gmail.com>
2018-02-27 18:07:42 +0100

    albus : update dexpreopt
    Change-Id: I619c1f8fd13a845610b0414f81c0983548680d8f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -139,7 +139,15 @@ BOARD_USES_QCNE := true
 # Crypto
 TARGET_HW_DISK_ENCRYPTION := true
 
-WITH_DEXPREOPT := true
+# Enable dexpreopt to speed boot time
+ifeq ($(HOST_OS),linux)
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+    endif
+  endif
+endif
 
 # Display
 BOARD_USES_ADRENO := true
