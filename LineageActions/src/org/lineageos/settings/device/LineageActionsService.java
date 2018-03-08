6be38fe4984fad07c53285ea334e13d1c2084b27
rahulsnair <rahulsnair30@gmail.com>
2018-03-08 20:49:29 +0100

    Revert "albus : disable glance"
    This reverts commit ce4fb52898d8c3f0a4736887a584b60cd76b5db4.

--- LineageActions/src/org/lineageos/settings/device/LineageActionsService.java
+++ LineageActions/src/org/lineageos/settings/device/LineageActionsService.java
@@ -35,7 +35,7 @@ import org.lineageos.settings.device.actions.LiftToSilence;
 import org.lineageos.settings.device.actions.ProximitySilencer;
 
 import org.lineageos.settings.device.doze.DozePulseAction;
-// import org.lineageos.settings.device.doze.GlanceSensor;
+import org.lineageos.settings.device.doze.GlanceSensor;
 import org.lineageos.settings.device.doze.ProximitySensor;
 import org.lineageos.settings.device.doze.ScreenReceiver;
 import org.lineageos.settings.device.doze.ScreenStateNotifier;
@@ -71,7 +71,7 @@ public class LineageActionsService extends IntentService implements ScreenStateN
         mScreenStateNotifiers.add(mDozePulseAction);
 
         // Actionable sensors get screen on/off notifications
-        // mScreenStateNotifiers.add(new GlanceSensor(lineageActionsSettings, mSensorHelper, mDozePulseAction));
+        mScreenStateNotifiers.add(new GlanceSensor(lineageActionsSettings, mSensorHelper, mDozePulseAction));
         mScreenStateNotifiers.add(new ProximitySensor(lineageActionsSettings, mSensorHelper, mDozePulseAction));
         mScreenStateNotifiers.add(new StowSensor(lineageActionsSettings, mSensorHelper, mDozePulseAction));
 
