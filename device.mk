12423fbd065213a057ee61657b54d49fc9f80eb3
rahulsnair <rahulsnair30@gmail.com>
2018-04-06 15:33:24 +0200

    albus : update camera
    Change-Id: I5726157a2ce0144cabf467c12212dbb3e79b62e9

--- device.mk
+++ device.mk
@@ -108,11 +108,12 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    libbson \
-    Camera2 \
-    camera.device@1.0-impl \
-    camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
+    camera.device@3.2-impl \
+    libbson \
+    libxml2 \
+    Camera2 \
     libshim_skia
 
 PRODUCT_COPY_FILES += \
