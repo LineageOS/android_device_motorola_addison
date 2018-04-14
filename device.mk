ae838814c938d1d2bf8d99a0aa0f698e6f3c2887
rahulsnair <rahulsnair30@gmail.com>
2018-04-14 10:32:50 +0200

    albus : update camera service
    Change-Id: Ia6e39519b5d1671e6d8637f75fbcf06892456f25

--- device.mk
+++ device.mk
@@ -109,6 +109,9 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0_vendor
     camera.device@3.2-impl \
     camera.device@1.0-impl \
     libxml2 \
