ad5dc69604a06d43a620667d299c0cfed15ae819
BtbN <btbn@btbn.de>
2018-07-11 21:58:12 +0200

    Don't use OPNS version
    Change-Id: If90f8f4d90d46bf7ec916e741009b25bff904cda

--- lineage.mk
+++ lineage.mk
@@ -29,6 +29,6 @@ PRODUCT_MANUFACTURER := motorola
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST := ro.product.model
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=motorola/addison/addison:8.0.0/OPNS27.76-12-22-3/3:user/release-keys \
-    PRIVATE_BUILD_DESC="addison-user 8.0.0 OPNS27.76-12-22-3 3 release-keys" \
+    BUILD_FINGERPRINT=motorola/addison/addison:8.0.0/OPN27.76-12-22/24:user/release-keys \
+    PRIVATE_BUILD_DESC="addison-user 8.0.0 OPN27.76-12-22 24 release-keys" \
     PRODUCT_NAME="Moto Z Play"
