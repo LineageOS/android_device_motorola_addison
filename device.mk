de090b7f4979aebf8d39f63b871766659886f393
BtbN <btbn@btbn.de>
2018-07-12 16:11:52 +0200

    Add telephony-ext dependency
    Change-Id: I3e23148af5b20c0174309fce3ba8e42aec7f2a9d

--- device.mk
+++ device.mk
@@ -277,6 +277,14 @@ PRODUCT_PACKAGES += \
     sensorhub.msm8953 \
     sensors.msm8953
 
+# Telephony
+PRODUCT_PACKAGES += \
+    ims-ext-common \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-addison.conf:system/etc/thermal-engine-addison.conf
