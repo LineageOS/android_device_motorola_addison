c055de88850fb9fca29e373c319244190ed615dd
rahulsnair <rahulsnair30@gmail.com>
2018-08-20 10:11:56 +0000

    albus : whitelist mod service in power save
    Change-Id: I81b8bd64fba8cd4b4ee145883f9e24dc4c3284ec

--- device.mk
+++ device.mk
@@ -298,6 +298,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     ModPermsFix
 
+# ModService
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/whitelist_modservice.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/whitelist_modservice.xml
+
 # Netutils
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.0 \
