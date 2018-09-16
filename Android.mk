rahulsnair/android_device_motorola_albus

albus : move firmware symlinks to vendor
Change-Id: I3941b9206ecbd8386492eb9dbb130a6b24a62605

Android.mk
@@ -20,7 +20,7 @@ LOCAL_PATH := $(call my-dir)
 FIRMWARE_ZAP_IMAGES := \
     a506_zap.b00 a506_zap.b01 a506_zap.b02 a506_zap.mdt
 
-FIRMWARE_ZAP_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_ZAP_IMAGES)))
+FIRMWARE_ZAP_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR_ETC)/firmware/,$(notdir $(FIRMWARE_ZAP_IMAGES)))
 $(FIRMWARE_ZAP_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "ZAP Firmware link: $@"
 	@mkdir -p $(dir $@)
@@ -34,7 +34,7 @@ FIRMWARE_ADSP_IMAGES := \
     adsp.b07 adsp.b08 adsp.b09 adsp.b10 adsp.b11 adsp.b12 adsp.b13 \
     adsp.mdt
 
-FIRMWARE_ADSP_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_ADSP_IMAGES)))
+FIRMWARE_ADSP_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR_ETC)/firmware/,$(notdir $(FIRMWARE_ADSP_IMAGES)))
 $(FIRMWARE_ADSP_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "ADSP Firmware link: $@"
 	@mkdir -p $(dir $@)
@@ -46,7 +46,7 @@ ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_ADSP_SYMLINKS)
 FIRMWARE_CMNLIB_IMAGES := \
     cmnlib.b00 cmnlib.b01 cmnlib.b02 cmnlib.b03 cmnlib.b04 cmnlib.b05 cmnlib.mdt
 
-FIRMWARE_CMNLIB_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_CMNLIB_IMAGES)))
+FIRMWARE_CMNLIB_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR_ETC)/firmware/,$(notdir $(FIRMWARE_CMNLIB_IMAGES)))
 $(FIRMWARE_CMNLIB_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Cmnlib Firmware link: $@"
 	@mkdir -p $(dir $@)
@@ -58,7 +58,7 @@ ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_CMNLIB_SYMLINKS)
 FIRMWARE_CMNLIB64_IMAGES := \
     cmnlib64.b00 cmnlib64.b01 cmnlib64.b02 cmnlib64.b03 cmnlib64.b04 cmnlib64.b05 cmnlib64.mdt
 
-FIRMWARE_CMNLIB64_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_CMNLIB64_IMAGES)))
+FIRMWARE_CMNLIB64_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR_ETC)/firmware/,$(notdir $(FIRMWARE_CMNLIB64_IMAGES)))
 $(FIRMWARE_CMNLIB64_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Cmnlib64 Firmware link: $@"
 	@mkdir -p $(dir $@)
@@ -71,7 +71,7 @@ FIRMWARE_CPE_IMAGES := \
     cpe_9335.b08 cpe_9335.b09 cpe_9335.b11 cpe_9335.b14 cpe_9335.b16 cpe_9335.b18 cpe_9335.b19 \
     cpe_9335.b20 cpe_9335.b22 cpe_9335.b24 cpe_9335.b26 cpe_9335.b28 cpe_9335.b29 cpe_9335.mdt
 
-FIRMWARE_CPE_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_CPE_IMAGES)))
+FIRMWARE_CPE_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR_ETC)/firmware/,$(notdir $(FIRMWARE_CPE_IMAGES)))
 $(FIRMWARE_CPE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "CPE Firmware link: $@"
 	@mkdir -p $(dir $@)
@@ -83,7 +83,7 @@ ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_CPE_SYMLINKS)
 FIRMWARE_CPPF_IMAGES := \
     cppf.b00 cppf.b01 cppf.b02 cppf.b03 cppf.b04 cppf.b05 cppf.b06 cppf.mdt
 
-FIRMWARE_CPPF_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_CPPF_IMAGES)))
+FIRMWARE_CPPF_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR_ETC)/firmware/,$(notdir $(FIRMWARE_CPPF_IMAGES)))
 $(FIRMWARE_CPPF_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Fingerprint Firmware link: $@"
 	@mkdir -p $(dir $@)
@@ -97,7 +97,7 @@ FIRMWARE_FINGERPRINT_IMAGES := \
     fpctzappfingerprint.b03 fpctzappfingerprint.b04 fpctzappfingerprint.b05 \
     fpctzappfingerprint.b06 fpctzappfingerprint.mdt
 
-FIRMWARE_FINGERPRINT_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_FINGERPRINT_IMAGES)))
+FIRMWARE_FINGERPRINT_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR_ETC)/firmware/,$(notdir $(FIRMWARE_FINGERPRINT_IMAGES)))
 $(FIRMWARE_FINGERPRINT_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Fingerprint Firmware link: $@"
 	@mkdir -p $(dir $@)
@@ -109,7 +109,7 @@ ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_FINGERPRINT_SYMLINKS)
 FIRMWARE_JSLR_IMAGES := \
     jslr.b00 jslr.b01 jslr.b02 jslr.b03 jslr.b04 jslr.b05 jslr.b06 jslr.mdt
 
-FIRMWARE_JSLR_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_JSLR_IMAGES)))
+FIRMWARE_JSLR_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR_ETC)/firmware/,$(notdir $(FIRMWARE_JSLR_IMAGES)))
 $(FIRMWARE_JSLR_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "JSLR firmware link: $@"
 	@mkdir -p $(dir $@)
@@ -120,7 +120,7 @@ ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_JSLR_SYMLINKS)
 
 MBA_IMAGES := mba.mbn
 
-MBA_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(MBA_IMAGES)))
+MBA_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR_ETC)/firmware/,$(notdir $(MBA_IMAGES)))
 $(MBA_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "MBA firmware link: $@"
 	@mkdir -p $(dir $@)
@@ -135,7 +135,7 @@ FIRMWARE_MODEM_IMAGES := \
     modem.b13 modem.b16 modem.b17 modem.b18 modem.b19 modem.b20 \
     modem.mdt
 
-FIRMWARE_MODEM_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_MODEM_IMAGES)))
+FIRMWARE_MODEM_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR_ETC)/firmware/,$(notdir $(FIRMWARE_MODEM_IMAGES)))
 $(FIRMWARE_MODEM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Modem Firmware link: $@"
 	@mkdir -p $(dir $@)
@@ -146,7 +146,7 @@ ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_MODEM_SYMLINKS)
 
 QDSP6M_IMAGES := qdsp6m.qdb
 
-QDSP6M_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(QDSP6M_IMAGES)))
+QDSP6M_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR_ETC)/firmware/,$(notdir $(QDSP6M_IMAGES)))
 $(QDSP6M_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "QDSP6M firmware link: $@"
 	@mkdir -p $(dir $@)
@@ -158,7 +158,7 @@ ALL_DEFAULT_INSTALLED_MODULES += $(QDSP6M_SYMLINKS)
 FIRMWARE_VENUS_IMAGES := \
     venus.b00 venus.b01 venus.b02 venus.b03 venus.b04 venus.mdt
 
-FIRMWARE_VENUS_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_VENUS_IMAGES)))
+FIRMWARE_VENUS_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR_ETC)/firmware/,$(notdir $(FIRMWARE_VENUS_IMAGES)))
 $(FIRMWARE_VENUS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Venus firmware link: $@"
 	@mkdir -p $(dir $@)
@@ -171,7 +171,7 @@ FIRMWARE_WCNSS_IMAGES := \
     wcnss.b00 wcnss.b01 wcnss.b02 wcnss.b04 wcnss.b06 \
     wcnss.b09 wcnss.b10 wcnss.b11 wcnss.b12 wcnss.mdt
 
-FIRMWARE_WCNSS_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_WCNSS_IMAGES)))
+FIRMWARE_WCNSS_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR_ETC)/firmware/,$(notdir $(FIRMWARE_WCNSS_IMAGES)))
 $(FIRMWARE_WCNSS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "WCNSS Firmware link: $@"
 	@mkdir -p $(dir $@)

LineageOS/android_device_motorola_addison

Android.mk
@@ -20,7 +20,7 @@ LOCAL_PATH := $(call my-dir)
 FIRMWARE_ZAP_IMAGES := \
     a506_zap.b00 a506_zap.b01 a506_zap.b02 a506_zap.mdt
 
-FIRMWARE_ZAP_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_ZAP_IMAGES)))
+FIRMWARE_ZAP_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(notdir $(FIRMWARE_ZAP_IMAGES)))
 $(FIRMWARE_ZAP_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "ZAP Firmware link: $@"
 	@mkdir -p $(dir $@)
@@ -34,7 +34,7 @@ FIRMWARE_ADSP_IMAGES := \
     adsp.b07 adsp.b08 adsp.b09 adsp.b10 adsp.b11 adsp.b12 adsp.b13 \
     adsp.mdt
 
-FIRMWARE_ADSP_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_ADSP_IMAGES)))
+FIRMWARE_ADSP_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(notdir $(FIRMWARE_ADSP_IMAGES)))
 $(FIRMWARE_ADSP_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "ADSP Firmware link: $@"
 	@mkdir -p $(dir $@)
@@ -46,7 +46,7 @@ ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_ADSP_SYMLINKS)
 FIRMWARE_CMNLIB_IMAGES := \
     cmnlib.b00 cmnlib.b01 cmnlib.b02 cmnlib.b03 cmnlib.b04 cmnlib.b05 cmnlib.mdt
 
-FIRMWARE_CMNLIB_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_CMNLIB_IMAGES)))
+FIRMWARE_CMNLIB_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(notdir $(FIRMWARE_CMNLIB_IMAGES)))
 $(FIRMWARE_CMNLIB_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Cmnlib Firmware link: $@"
 	@mkdir -p $(dir $@)
@@ -58,7 +58,7 @@ ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_CMNLIB_SYMLINKS)
 FIRMWARE_CMNLIB64_IMAGES := \
     cmnlib64.b00 cmnlib64.b01 cmnlib64.b02 cmnlib64.b03 cmnlib64.b04 cmnlib64.b05 cmnlib64.mdt
 
-FIRMWARE_CMNLIB64_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_CMNLIB64_IMAGES)))
+FIRMWARE_CMNLIB64_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(notdir $(FIRMWARE_CMNLIB64_IMAGES)))
 $(FIRMWARE_CMNLIB64_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Cmnlib64 Firmware link: $@"
 	@mkdir -p $(dir $@)
@@ -71,7 +71,7 @@ FIRMWARE_CPE_IMAGES := \
     cpe_9335.b08 cpe_9335.b09 cpe_9335.b11 cpe_9335.b14 cpe_9335.b16 cpe_9335.b18 cpe_9335.b19 \
     cpe_9335.b20 cpe_9335.b22 cpe_9335.b24 cpe_9335.b26 cpe_9335.b28 cpe_9335.b29 cpe_9335.mdt
 
-FIRMWARE_CPE_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_CPE_IMAGES)))
+FIRMWARE_CPE_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(notdir $(FIRMWARE_CPE_IMAGES)))
 $(FIRMWARE_CPE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "CPE Firmware link: $@"
 	@mkdir -p $(dir $@)
@@ -83,7 +83,7 @@ ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_CPE_SYMLINKS)
 FIRMWARE_CPPF_IMAGES := \
     cppf.b00 cppf.b01 cppf.b02 cppf.b03 cppf.b04 cppf.b05 cppf.b06 cppf.mdt
 
-FIRMWARE_CPPF_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_CPPF_IMAGES)))
+FIRMWARE_CPPF_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(notdir $(FIRMWARE_CPPF_IMAGES)))
 $(FIRMWARE_CPPF_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Fingerprint Firmware link: $@"
 	@mkdir -p $(dir $@)
@@ -97,7 +97,7 @@ FIRMWARE_FINGERPRINT_IMAGES := \
     fpctzappfingerprint.b03 fpctzappfingerprint.b04 fpctzappfingerprint.b05 \
     fpctzappfingerprint.b06 fpctzappfingerprint.mdt
 
-FIRMWARE_FINGERPRINT_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_FINGERPRINT_IMAGES)))
+FIRMWARE_FINGERPRINT_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(notdir $(FIRMWARE_FINGERPRINT_IMAGES)))
 $(FIRMWARE_FINGERPRINT_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Fingerprint Firmware link: $@"
 	@mkdir -p $(dir $@)
@@ -109,7 +109,7 @@ ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_FINGERPRINT_SYMLINKS)
 FIRMWARE_JSLR_IMAGES := \
     jslr.b00 jslr.b01 jslr.b02 jslr.b03 jslr.b04 jslr.b05 jslr.b06 jslr.mdt
 
-FIRMWARE_JSLR_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_JSLR_IMAGES)))
+FIRMWARE_JSLR_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(notdir $(FIRMWARE_JSLR_IMAGES)))
 $(FIRMWARE_JSLR_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "JSLR firmware link: $@"
 	@mkdir -p $(dir $@)
@@ -120,7 +120,7 @@ ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_JSLR_SYMLINKS)
 
 MBA_IMAGES := mba.mbn
 
-MBA_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(MBA_IMAGES)))
+MBA_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(notdir $(MBA_IMAGES)))
 $(MBA_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "MBA firmware link: $@"
 	@mkdir -p $(dir $@)
@@ -135,7 +135,7 @@ FIRMWARE_MODEM_IMAGES := \
     modem.b13 modem.b16 modem.b17 modem.b18 modem.b19 modem.b20 \
     modem.mdt
 
-FIRMWARE_MODEM_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_MODEM_IMAGES)))
+FIRMWARE_MODEM_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(notdir $(FIRMWARE_MODEM_IMAGES)))
 $(FIRMWARE_MODEM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Modem Firmware link: $@"
 	@mkdir -p $(dir $@)
@@ -146,7 +146,7 @@ ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_MODEM_SYMLINKS)
 
 QDSP6M_IMAGES := qdsp6m.qdb
 
-QDSP6M_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(QDSP6M_IMAGES)))
+QDSP6M_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(notdir $(QDSP6M_IMAGES)))
 $(QDSP6M_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "QDSP6M firmware link: $@"
 	@mkdir -p $(dir $@)
@@ -158,7 +158,7 @@ ALL_DEFAULT_INSTALLED_MODULES += $(QDSP6M_SYMLINKS)
 FIRMWARE_VENUS_IMAGES := \
     venus.b00 venus.b01 venus.b02 venus.b03 venus.b04 venus.mdt
 
-FIRMWARE_VENUS_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_VENUS_IMAGES)))
+FIRMWARE_VENUS_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(notdir $(FIRMWARE_VENUS_IMAGES)))
 $(FIRMWARE_VENUS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Venus firmware link: $@"
 	@mkdir -p $(dir $@)
@@ -171,7 +171,7 @@ FIRMWARE_WCNSS_IMAGES := \
     wcnss.b00 wcnss.b01 wcnss.b02 wcnss.b04 wcnss.b06 \
     wcnss.b09 wcnss.b10 wcnss.b11 wcnss.b12 wcnss.mdt
 
-FIRMWARE_WCNSS_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_WCNSS_IMAGES)))
+FIRMWARE_WCNSS_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR_ETC)/firmware/,$(notdir $(FIRMWARE_WCNSS_IMAGES)))
 $(FIRMWARE_WCNSS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "WCNSS Firmware link: $@"
 	@mkdir -p $(dir $@)