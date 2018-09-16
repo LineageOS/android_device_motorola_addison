rahulsnair/android_device_motorola_albus

albus: split vendor specific and system props
Change-Id: Ie3e152742af423c58ca5a3478b722af81e5a4241

BoardConfig.mk
@@ -249,3 +249,5 @@ WPA_SUPPLICANT_VERSION           := VER_0_8_X
 TARGET_COPY_OUT_VENDOR := vendor
 BOARD_VENDORIMAGE_PARTITION_SIZE := 665845760
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
+TARGET_VENDOR_PROP += $(PLATFORM_PATH)/vendor.prop

LineageOS/android_device_motorola_addison

BoardConfig.mk
@@ -262,6 +262,8 @@ PRODUCT_VENDOR_MOVE_ENABLED := true
 PRODUCT_FULL_TREBLE_OVERRIDE := true
 PRODUCT_SHIPPING_API_LEVEL := 25
 PRODUCT_COMPATIBILITY_MATRIX_LEVEL_OVERRIDE := 27
+BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Wifi
 BOARD_HAS_QCOM_WLAN              := true