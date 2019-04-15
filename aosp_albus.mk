9e412f25fc94c82ae2631e02046026543bd17aa0
Jiyong Park <jiyong@google.com>
2019-04-15 14:19:03 +0900

    albus: Add updatable_apex.mk
    The product abstracts configs that need to be set to support updating of APEXes.
    
    Change-Id: Id5aa944ceba6a6846308f1c18a08af8b3bb38589

--- aosp_albus.mk
+++ aosp_albus.mk
@@ -24,6 +24,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Inherit some common AEX stuff.
 $(call inherit-product, vendor/aosp/common.mk)
 
