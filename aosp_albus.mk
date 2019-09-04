a0c93a3fe2f276c4e9ea1b0c52a7045deb8592ed
Erfan Abdi <erfangplus@gmail.com>
2019-09-04 17:14:27 +0430

    albus: inherit product api level makefile
    Change-Id: I970ef7f9a0d352a286395c7cad79dcaf3245f0ab
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- aosp_albus.mk
+++ aosp_albus.mk
@@ -22,7 +22,7 @@ $(call inherit-product, device/motorola/albus/full_albus.mk)
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
 # Inherit some common AEX stuff.
 $(call inherit-product, vendor/aosp/common.mk)
