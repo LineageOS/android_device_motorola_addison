d68f097780fea9a2eeaa419f4ff86f065de34f1e
Erfan Abdi <erfangplus@gmail.com>
2019-09-09 02:23:14 +0430

    albus: Build AOSP camera provider service
    * But use moto camera.device@1.0-impl.so
    
    Change-Id: I8a907de60f9ae4c903740282253b91fe8e5bc273
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- device.mk
+++ device.mk
@@ -113,6 +113,7 @@ PRODUCT_PACKAGES += \
 #Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor \
     camera.device@3.2-impl \
