735ab9a2ab4440d4a69c24a2d5570c9dd9800a1b
Dante Russo <drusso@codeaurora.org>
2019-11-20 14:48:55 -0800

    albus: gps: Remove libhidltransport & libhwbinder references
    These libraries are no longer required and
    references are removed for optimization.
    
    Change-Id: I186befda34a7d53a3c8f1e80449639dd230b1e1b
    CRs-fixed: 2559079

--- gps/android/2.0/Android.mk
+++ gps/android/2.0/Android.mk
@@ -44,8 +44,6 @@ LOCAL_HEADER_LIBRARIES := \
 LOCAL_SHARED_LIBRARIES := \
     liblog \
     libhidlbase \
-    libhidltransport \
-    libhwbinder \
     libcutils \
     libutils \
     android.hardware.gnss@1.0 \
@@ -98,9 +96,7 @@ LOCAL_SHARED_LIBRARIES := \
     libqti_vndfwk_detect \
 
 LOCAL_SHARED_LIBRARIES += \
-    libhwbinder \
     libhidlbase \
-    libhidltransport \
     android.hardware.gnss@1.0 \
     android.hardware.gnss@1.1 \
     android.hardware.gnss@2.0 \
