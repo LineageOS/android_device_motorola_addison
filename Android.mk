2a89d5b894c10381097d63dfb16014119f76c23b
Erfan Abdi <erfangplus@gmail.com>
2020-01-28 11:49:38 +0330

    albus: Drop root symlinks to firmwares
    Change-Id: I3220b4dcc80baf01721de9b948de99b9140b72e8

--- Android.mk
+++ Android.mk
@@ -30,16 +30,10 @@ ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_MOUNT_POINT) \
 $(FIRMWARE_MOUNT_POINT):
 	@echo "Creating $(FIRMWARE_MOUNT_POINT)"
 	@mkdir -p $(TARGET_OUT_VENDOR)/firmware_mnt
-ifneq ($(TARGET_MOUNT_POINTS_SYMLINKS),false)
-	@ln -sf /vendor/firmware_mnt $(TARGET_ROOT_OUT)/firmware
-endif
 
 $(DSP_MOUNT_POINT):
 	@echo "Creating $(DSP_MOUNT_POINT)"
 	@mkdir -p $(TARGET_OUT_VENDOR)/dsp
-ifneq ($(TARGET_MOUNT_POINTS_SYMLINKS),false)
-	@ln -sf /vendor/dsp $(TARGET_ROOT_OUT)/dsp
-endif
 
 $(FSG_MOUNT_POINT):
 	@echo "Creating $(FSG_MOUNT_POINT)"
