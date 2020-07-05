ca17b2a7a1881c70d8797156ccabc48055a51079
EmaMaker <emamaker0@gmail.com>
2020-07-05 14:27:46 +0200

    correctly rename services for light hal
    now light hal and screen brightness work

--- light/service.cpp
+++ light/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.light@2.0-service.potter"
+#define LOG_TAG "android.hardware.light@2.0-service.albus"
 
 #include <hidl/HidlTransportSupport.h>
 
