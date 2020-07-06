d435ef18b0dd9e53bc3fcf0aca99b773ae798a09
Marcos Tolcachir <marcostolcachir@gmail.com>
2020-07-06 09:33:14 -0300

    Fix gps mismatches

--- gps/pla/Android.mk
+++ gps/pla/Android.mk
@@ -16,9 +16,6 @@ GNSS_CFLAGS := \
     -Wno-error=switch \
     -Wno-error=date-time
 
-ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
-ifneq ($(BUILD_TINY_ANDROID),true)
-
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -26,5 +23,3 @@ LOCAL_MODULE := libloc_pla_headers
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/android
 include $(BUILD_HEADER_LIBRARY)
 
-endif # not BUILD_TINY_ANDROID
-endif # BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE
