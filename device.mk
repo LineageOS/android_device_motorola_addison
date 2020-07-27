a6e712a109ce653495e4134195fbf79866a6619c
EmaMaker <emamaker0@gmail.com>
2020-07-27 17:16:26 +0200

    fix unneeded and duplicated .xml config files for audio also kang audio_policy_configuration.xml from potter now BT audio works on Q GSIs too
    Change-Id: Ib5b0d71e7749eb98781d8b0465b57bd82dd88192

--- device.mk
+++ device.mk
@@ -89,9 +89,7 @@ PRODUCT_PACKAGES += \
     libasphere
 
 PRODUCT_COPY_FILES +=  \
-    $(LOCAL_PATH)/audio/audio_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
-    $(LOCAL_PATH)/audio/audio_effects.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.conf \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
     $(LOCAL_PATH)/audio/audio_platform_info_extcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_extcodec.xml \
