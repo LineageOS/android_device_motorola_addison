1b802fbb6e02dc1e2d16b0a0418c41e89375bba6
EmaMaker <emamaker0@gmail.com>
2020-08-12 16:05:04 +0200

    albus: update Radio, IMS, CNE and QMI blobs From deen PPKS29.68 *Upver stuff in manifest and and missing ones *New proprietary-files to match our new blobs
    Change-Id: I2c9a7f0d349a8049dd862c5c0f79ef92f8032470

--- config.fs
+++ config.fs
@@ -40,6 +40,12 @@ group: AID_RADIO
 mode: 0755
 caps: NET_BIND_SERVICE
 
+[vendor/bin/imsrcsd]
+mode: 0755
+user: AID_SYSTEM
+group: AID_RADIO
+caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
+
 [vendor/bin/pm-service]
 user: AID_SYSTEM
 group: AID_SYSTEM
