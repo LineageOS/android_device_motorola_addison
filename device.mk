a7285ab6a0201aeb30f3804a97b153b172ba9feb
Arian <arian.kulmer@web.de>
2020-09-22 20:06:14 +0200

    albus: Build new aidl qti power HAL
    Change-Id: I4bdea354e54d34d0dfe9b811e228fc6f8762d2ed

--- device.mk
+++ device.mk
@@ -283,7 +283,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power-service-qti
     
 # Powerhint configuration file
 PRODUCT_COPY_FILES += \
