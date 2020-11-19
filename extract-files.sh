4edb2c6775deb2c3ab1b6cabd3c39b438fc111ce
Bruno Martins <bgcngm@gmail.com>
2020-11-19 00:32:09 +0000

    albus: Remove unused jars and radio libs
    Change-Id: I651edb4823e54b589b7b72f6362a07c6e78b955a

--- extract-files.sh
+++ extract-files.sh
@@ -72,12 +72,7 @@ function blob_fixup() {
             "${PATCHELF}" --add-needed "libcutils_shim.so" "$LIBCUTILS_SHIM"
         done
     ;;
-
-    # Fix xml version
-    product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml | product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml)
-        sed -i 's/xml version="2.0"/xml version="1.0"/' "${2}"
-        ;;
-
+    
     #libwui patch
     vendor/lib/libmot_gpu_mapper.so)
         sed -i "s/libgui/libwui/" "${2}"
