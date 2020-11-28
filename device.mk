79c6934fcae3a9ff95df608459d0a39174fbb814
Bruno Martins <bgcngm@gmail.com>
2020-11-28 02:32:43 +0000

    addison: Drop obsolete priv-app permissions
    Change-Id: I42e69b8e1d9caf8d580176734072e05fad7f8725

--- device.mk
+++ device.mk
@@ -326,7 +326,6 @@ PRODUCT_PACKAGES += \
 
 # Qualcomm
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/telephony_product_privapp-permissions-qti.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/telephony_product_privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
