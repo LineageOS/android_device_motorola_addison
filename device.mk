0b02b4e18f5487a2c8281e080e0d598a96845cfb
Nolen Johnson <johnsonnolen@gmail.com>
2021-01-03 12:58:57 -0500

    albus: Use hardware/motorola's common MotoActions
    Change-Id: Ic730e45b25c87fb5b9d8d301bf44bd7176a0576c

--- device.mk
+++ device.mk
@@ -231,7 +231,11 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
-# ModService
+# MotoActions
+PRODUCT_PACKAGES += \
+    MotoActions
+
+# Modservice
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/whitelist_modservice.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/whitelist_modservice.xml
 
