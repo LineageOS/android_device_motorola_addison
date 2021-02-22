18f9dbf6991df39ba89156f2b30a0e8a59469ff0
marcost2 <marcostolcachir@gmail.com>
2021-02-22 09:41:57 -0300

    albus: Align extract utils with templates

--- extract-files.sh
+++ extract-files.sh
@@ -23,86 +23,93 @@ VENDOR=motorola
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-LINEAGE_ROOT="$MY_DIR"/../../..
+AICP_ROOT="${MY_DIR}/../../.."
 
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
+HELPER="${AICP_ROOT}/vendor/aicp/build/tools/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
-. "$HELPER"
+source "${HELPER}"
+
+# Default to sanitizing the vendor folder before extraction
+CLEAN_VENDOR=true
+SECTION=
+KANG=
 
 while [ "$1" != "" ]; do
-    case $1 in
+    case "$1" in
         -n | --no-cleanup )     CLEAN_VENDOR=false
                                 ;;
+        -k | --kang)            KANG="--kang"
+                                ;;
         -s | --section )        shift
-                                SECTION=$1
+                                SECTION="$1"
                                 CLEAN_VENDOR=false
                                 ;;
-        * )                     SRC=$1
+        * )                     SRC="$1"
                                 ;;
     esac
     shift
 done
 
-if [ -z "$SRC" ]; then
+if [ -z "${SRC}" ]; then
     SRC=adb
 fi
 
+function blob_fixup() {
+    case "${1}" in
+
+    # Fix xml version
+    product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml | product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml)
+        sed -i 's/xml version="2.0"/xml version="1.0"/' "${2}"
+        ;;
+
+    # Fix fingerprint UHID
+    vendor/etc/init/android.hardware.biometrics.fingerprint@2.1-service.rc)
+        sed -i 's/group system input 9015/group system uhid input 9015/' "${2}"
+        ;;
+
+    #libwui patch
+    vendor/lib/libmot_gpu_mapper.so)
+        sed -i "s/libgui/libwui/" "${2}"
+        ;;
+
+    vendor/lib/libmmcamera2_sensor_modules.so)
+        sed -i "s|/system/etc/camera/|/vendor/etc/camera/|g" "${2}"
+        ;;
+    
+    vendor/lib/libmmcamera_vstab_module.so | vendor/lib/libmmcamera2_stats_modules.so)
+        patchelf --remove-needed libandroid.so "${2}"
+        ;;
+
+    vendor/lib/lib_mottof.so | vendor/lib/libmmcamera_vstab_module.so | vendor/lib/libjscore.so | vendor/lib/libmmcamera_ppeiscore.so | vendor/lib/libmmcamera2_stats_modules.so)
+        sed -i "s/libgui/libwui/" "${2}"
+        ;;
+    
+    vendor/lib/libcamerabgprocservice.so)
+        patchelf --remove-needed libcamera_client.so "${2}"
+        ;;
+    
+    # Patch libcutils dep into audio HAL
+    vendor/lib/hw/audio.primary.msm8953.so)
+        patchelf --replace-needed "libcutils.so" "libprocessgroup.so" "${2}"
+        ;;
+
+    vendor/lib/hw/camera.msm8953.so)
+        sed -i "s|service.bootanim.exit|service.bootanim.hold|g" "${2}"
+        ;;
+
+    vendor/lib/libzaf_core.so)
+        sed -i "s|/system/etc/zaf|/vendor/etc/zaf|g" "${2}"
+    esac
+}
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
+setup_vendor "${DEVICE}" "${VENDOR}" "${AICP_ROOT}" false "${CLEAN_VENDOR}"
 
-extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
-
-BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
-
-vendor/lib/hw/camera.msm8996.so)
-    sed -i "s|service.bootanim.exit|service.bootanim.hold|g" "${2}"
-    ;;
-
-readonly MMCAMERA=(
-   vendor/lib/libmmcamera_vstab_module.so
-   vendor/lib/libmmcamera2_stats_modules.so
-)
-
-for i in "${MMCAMERA[@]}"; do
-  patchelf --remove-needed libandroid.so "$BLOB_ROOT"/${i}
-done
-
-# Load camera configs from vendor
-CAMERA2_SENSOR_MODULES="$BLOB_ROOT"/vendor/lib/libmmcamera2_sensor_modules.so
-sed -i "s|/system/etc/camera/|/vendor/etc/camera/|g" "$CAMERA2_SENSOR_MODULES"
-
-PROC_SERVICE="$BLOB_ROOT"/vendor/lib/libcamerabgprocservice.so
-patchelf --remove-needed libcamera_client.so "$PROC_SERVICE"
-
-readonly LIBWUI_FIXUP=(
-   vendor/lib/libmmcamera_vstab_module.so
-   vendor/lib/libmmcamera_ppeiscore.so
-   vendor/lib/libmmcamera2_stats_modules.so
-   vendor/lib/libjscore.so
-   vendor/lib/lib_mottof.so
-)
-
-for i in "${LIBWUI_FIXUP[@]}"; do
-  sed -i "s/libgui/libwui/" "$BLOB_ROOT"/${i}
-done
-
-# Load ZAF configs from vendor
-ZAF_CORE="$BLOB_ROOT"/vendor/lib/libzaf_core.so
-sed -i "s|/system/etc/zaf|/vendor/etc/zaf|g" "$ZAF_CORE"
+extract "${MY_DIR}/proprietary-files.txt" "${SRC}" ${KANG} --section "${SECTION}"
 
 
-# Load camera metadata shim
-CAMERAHAL="$BLOB_ROOT"/vendor/lib/hw/camera.msm8953.so
-patchelf --replace-needed libcamera_client.so libcamera_metadata_helper.so "$CAMERAHAL"
-
-# Patch libcutils dep into audio HAL
-vendor/lib/hw/audio.primary.msm8953.so)
-    patchelf --replace-needed "libcutils.so" "libprocessgroup.so" "${2}"
-    ;;
-
-"$MY_DIR"/setup-makefiles.sh
+"${MY_DIR}/setup-makefiles.sh"
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -27,9 +27,9 @@ INITIAL_COPYRIGHT_YEAR=2017
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-LINEAGE_ROOT="$MY_DIR"/../../..
+AICP_ROOT="$MY_DIR"/../../..
 
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="$AICP_ROOT"/vendor/aicp/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -37,16 +37,12 @@ fi
 . "$HELPER"
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
+setup_vendor "$DEVICE" "$VENDOR" "$AICP_ROOT"
 
 # Copyright headers and guards
 write_headers
 
-write_makefiles "$MY_DIR"/proprietary-files.txt true
-
-cat << EOF >> "$ANDROIDMK"
-
-EOF
+write_makefiles "$MY_DIR"/proprietary-files.txt
 
 # Finish
 write_footers
