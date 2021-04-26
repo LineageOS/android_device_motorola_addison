b4fda7d9009a10f2041b5a9c94abbcf18e501b0c
dianlujitao <dianlujitao@lineageos.org>
2021-04-26 20:39:23 +0800

    Revert "albus: Switch to Audio HAL V6"
    All target devices use pre-R audio.bluetooth_qti.default.so and
    pre-R audio hal, in which the new set_event_callback API is not
    implemented and the audio service will crash due to mismatched struct.
    
    Also remove soundtrigger
    This reverts commit c8455265e0eab2015ca5288620ca80e2f3626dc9.
    
    Change-Id: Ia330ac05d84d3dfb3a639458d66a3dc29689e67c

--- device.mk
+++ device.mk
@@ -33,10 +33,9 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@6.0-impl \
+    android.hardware.audio@5.0-impl \
     android.hardware.audio.service \
-    android.hardware.audio.effect@6.0-impl \
-    android.hardware.soundtrigger@2.2-impl \
+    android.hardware.audio.effect@5.0-impl \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
