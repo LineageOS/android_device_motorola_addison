699bc772b12c11bff693e14dbaceea01d3ff0928
Nicholas Lim <nicknitewolf@lineageos.org>
2021-05-21 16:56:47 +0800

    albus: Move pocketmode to vendor
     * For better GSI support
    
    Change-Id: I3f88aa8d3b017bfa81c3594f88bd42d586f9db5e

--- pocketmode/Android.mk
+++ pocketmode/Android.mk
@@ -8,7 +8,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := MotoPocketMode
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVATE_PLATFORM_APIS := true
-LOCAL_PRIVILEGED_MODULE := true
+LOCAL_VENDOR_MODULE := true
 LOCAL_REQUIRED_MODULES := pocketmode_whitelist.xml
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
@@ -20,5 +20,6 @@ LOCAL_MODULE := pocketmode_whitelist.xml
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_RELATIVE_PATH := sysconfig
+LOCAL_VENDOR_MODULE := true
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
