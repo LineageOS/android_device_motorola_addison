46f0011db43c66afd9978656162b2c1b023d6223
Mathieu Chartier <mathieuc@google.com>
2021-08-22 16:32:42 +0800

    addison: Always preopt extracted APKs
    Change-Id: I2e4742a796b8142f14eaad5eba81eaf800d64914

--- device.mk
+++ device.mk
@@ -391,6 +391,7 @@ PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
 
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     Settings \
