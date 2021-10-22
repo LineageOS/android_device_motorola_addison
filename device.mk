19aff195c1253564cf4cdee7acaada3a8d9bf59d
marcost2 <marcostolcachir@gmail.com>
2021-10-22 15:42:54 -0300

    addison: Drop Touch HAL
    * Addison can't get fp navigation, so it makes little sense to have it

--- device.mk
+++ device.mk
@@ -200,10 +200,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.addison
 
-# Lineage hardware
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.addison
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
