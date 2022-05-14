a23672db92687c1b8d6fe3eed207c439c2a3df22
marcost2 <marcostolcachir@gmail.com>
2022-05-14 10:23:57 -0300

    addison: Add support for chinese variant's nfc
    Change-Id: Ia2ae0a816ab3e8678ef1143ec728e8b65c9fed19

--- device.mk
+++ device.mk
@@ -234,7 +234,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
-    $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
+    $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
+    $(LOCAL_PATH)/configs/nfc/libnfc-nxp_retcn.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp_retcn.conf
 
 # Net
 PRODUCT_PACKAGES += \
